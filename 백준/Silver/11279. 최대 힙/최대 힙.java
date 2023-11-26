b7fe65b4fb313b7fa00a4768d06562e9a7124d4e
banseok1216 <114564687+banseok1216@users.noreply.github.com>
2023-11-26 22:49:49 +0900

    [Silver II] Title: 최대 힙, Time: 468 ms, Memory: 29104 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    static StringBuilder stringBuilder = new StringBuilder();
+    static int[] treeArray_1;
+    static int[] treeArray_2;
+    static int[] abc;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+    static int[][][] visited;
+    static int[][] v;
+    static boolean checkBipartite;
+    static String K = "YES";
+    static int R = 0;
+    static int p = 0;
+    static int q = 0;
+    static LinkedList<int []> Queue = new LinkedList<int[]>();
+    static LinkedList<Integer> list = new LinkedList<>();
+    static LinkedList<Integer> list1 = new LinkedList<>();
+    static Deque<Integer> deque1 = new ArrayDeque<Integer>();
+    static Deque<Integer> deque2 = new ArrayDeque<Integer>();
+    static int[] list2;
+    static Stack<Integer> stack = new Stack<>();
+    static Stack<String> stack1 = new Stack<>();
+    static Queue<Integer> queue1 = new LinkedList<Integer>();
+    static Queue<Integer> queuey = new LinkedList<Integer>();
+    static HashSet<Integer> ok = new HashSet<>();
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static PriorityQueue<Integer> priorityQueueHighest = new PriorityQueue<>(Collections.reverseOrder());
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        Scanner sc = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        int m = Integer.parseInt(br.readLine());
+        for(int i=0; i<m; i++){
+            int n= Integer.parseInt(br.readLine());
+            if(n==0){
+                if(priorityQueueHighest.isEmpty()){
+                    stringBuilder.append(0).append("\n");
+                }
+                else{
+                    stringBuilder.append(priorityQueueHighest.poll()).append("\n");
+                }
+            }
+            else{
+                priorityQueueHighest.add(n);
+            }
+        }
+        System.out.println(stringBuilder);
+    }
+}
