1813a5f3dcbbb00e1aad75979b347fdc42e29dab
banseok1216 <114564687+banseok1216@users.noreply.github.com>
2023-12-02 22:59:23 +0900

    [Gold II] Title: 보석 도둑, Time: 1880 ms, Memory: 112804 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Jewelry {
+    int mass; // 무게
+    int value; // 가격
+
+    Jewelry(int mass, int value) {
+        this.mass = mass;
+        this.value = value;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Jewelry[] jewelries = new Jewelry[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            jewelries[i] = new Jewelry(m, v);
+        }
+        // 무게를 오름차순 정렬하되, 무게가 같을 경우 가격을 내림차순 정렬.
+        Arrays.sort(jewelries, new Comparator<Jewelry>() {
+
+            @Override
+            public int compare(Jewelry o1, Jewelry o2) {
+                if (o1.mass == o2.mass) {
+                    return o2.value - o1.value;
+                }
+                return o1.mass - o2.mass;
+            }
+
+        });
+
+        int[] bags = new int[K];
+        for (int i = 0; i < K; i++) {
+            bags[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(bags);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        long ans = 0;
+        for (int i = 0, j = 0; i < K; i++) {
+            while (j < N && jewelries[j].mass <= bags[i]) {
+                pq.offer(jewelries[j++].value);
+            }
+            if (!pq.isEmpty()) {
+                ans += pq.poll();
+            }
+        }
+        System.out.println(ans);
+    }
+}
