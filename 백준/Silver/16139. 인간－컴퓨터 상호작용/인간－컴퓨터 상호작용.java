d8db83baf11c5b898315b8355e09a92e42ed36de
banseok1216 <114564687+banseok1216@users.noreply.github.com>
2024-01-05 15:24:55 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 860 ms, Memory: 108852 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -4,66 +4,35 @@ import java.io.*;
 import java.util.*;
 import java.lang.*;
 
-public class Main {
-    static StringBuilder stringBuilder = new StringBuilder();
-    static int[] treeArray_1;
-    static char[] treeArray_2;
-    static int[] abc;
-    static int max = 0;
-    static int min =1000000000;
-    static boolean[] visited;
-    static long[][] dp;
-    static int K=0;
-    static int p;
-    static int q;
-    static Queue<Integer> Queue = new LinkedList<Integer>();
-    static Deque<Integer> deque1 = new ArrayDeque<Integer>();
-    static Deque<Integer> deque2 = new ArrayDeque<Integer>();
-    static String [][] list;
-    static Stack<Integer> stack = new Stack<Integer>();
-    static Queue<int []> queue = new LinkedList<int[]>();
+class Main {
+    static int m, n, k;
+    static int max;
+    static ArrayList<Integer> tree = new ArrayList<>();
+    static ArrayList<Integer> ans = new ArrayList<>();
+    static int[][] v;
+    static int[] v1, arr, arr2;
+    static String[] strarr;
+    static String[] sum;
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] check;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        Scanner sc = new Scanner(System.in);
-        StringBuilder stringBuilder = new StringBuilder();
-        String str1 = br.readLine();
-        int k[][] = new int[26][str1.length()+1];
-        int m = Integer.parseInt(br.readLine());
-
-        for (int i=1; i<str1.length()+1; i++){
-            char c = str1.charAt(i-1);
-            for(int j=0; j<26; j++){
-                if(i==1){
-                    if(c - 97 == j){
-                        k[j][i] = 1;
-                    }
-                    else {
-                        k[j][i] = 0;
-                    }
-                }
-                else if(c - 97 == j) {
-                    k[c - 97][i] = k[c - 97][i-1] + 1;
-                }
-                else{
-                    k[j][i] = k[j][i-1];
-                }
+        String s = br.readLine();
+        int m =Integer.parseInt(br.readLine());
+        v = new int[26][200001];
+        for(int i=0; i<s.length(); i++){
+            v[s.charAt(i)-'a'][i+1] ++;
+        }
+        for(int j=1; j<200001; j++){
+            for(int i=0; i<26; i++){
+                v[i][j] += v[i][j-1];
             }
         }
         for(int i=0; i<m; i++){
-            String[] str2 = br.readLine().split(" ");
-            int s;
-            if(Integer.parseInt(str2[1]) ==0){
-                s = k[str2[0].charAt(0)-97][Integer.parseInt(str2[2])+1];
-            }
-            else {
-                s = k[str2[0].charAt(0) - 97][Integer.parseInt(str2[2])+1] - k[str2[0].charAt(0) - 97][Integer.parseInt(str2[1])];
-            }
-            stringBuilder.append(s).append("\n");
+            String[] str = br.readLine().split(" ");
+            sb.append(v[str[0].charAt(0)-'a'][Integer.parseInt(str[2])+1]-v[str[0].charAt(0)-'a'][Integer.parseInt(str[1])]).append("\n");
         }
-        System.out.println(stringBuilder);
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
 }
