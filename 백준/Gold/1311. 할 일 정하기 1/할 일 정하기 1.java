96a22d8112d5ec6041c44c397c6fd3eb85dbcd2e
banseok1216 <114564687+banseok1216@users.noreply.github.com>
2024-01-07 16:37:50 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 256 ms, Memory: 18364 KB -BaekjoonHub

--- 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.java
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.java
@@ -1,49 +1,110 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.*;
 import java.util.*;
-import java.lang.*;
 
-class Main {
-    static int m, n, k;
-    static int max;
-    static ArrayList<Integer> tree = new ArrayList<>();
-    static ArrayList<Integer> ans = new ArrayList<>();
-    static int[][] v,arr;
-    static int[] v1, arr2;
-    static String[] strarr;
-    static String[] sum;
-    static StringBuilder sb = new StringBuilder();
-    static boolean[][] check;
+class Edge {
+    public int v, c, f, w;
+    public Edge rev;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        m = Integer.parseInt(br.readLine());
-        v= new int[m][m];
-        arr = new int[1<<m][m+1];
-        for(int i=0; i<(1<<m); i++) Arrays.fill(arr[i],Integer.MAX_VALUE);
-        for (int i = 0; i < m; i++) {
-            String[] str1 = br.readLine().split(" ");
-            for (int j = 0; j < m; j++) {
-                v[i][j] = Integer.parseInt(str1[j]);
-            }
-        }
-        int ans=dfs(0,0);
-        System.out.println(ans);
+    public Edge(int v, int c, int w) {
+        this.v = v;
+        this.c = c;
+        this.f = 0;
+        this.w = w;
     }
-    private static int dfs(int count,int bit){
-        if(count == m){
-            return 0;
+
+    public boolean pos() {
+        return c > f;
+    }
+
+    public void flow(int mf) {
+        f += mf;
+        rev.f -= mf;
+    }
+}
+
+public class Main {
+    static List<Edge>[] con;
+    static int n;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        con = new ArrayList[42];
+        for (int i = 0; i < 42; i++) {
+            con[i] = new ArrayList<>();
         }
-        if(arr[bit][count] !=Integer.MAX_VALUE){
-            return arr[bit][count];
-        }
-        for(int i=0; i<m; i++){
-            if((bit&(1<<i)) == 0){
-                int bitset = bit|(1<<i);
-                arr[bit][count] = Math.min(arr[bit][count],v[count][i]+dfs(count+1, bitset));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int w = scanner.nextInt();
+                addedge(i, j + n, w);
             }
         }
-        return arr[bit][count];
+
+        int src = 2 * n;
+        int snc = 2 * n + 1;
+
+        for (int i = 0; i < n; i++)
+            addedge(src, i, 0);
+
+        for (int i = 0; i < n; i++)
+            addedge(i + n, snc, 0);
+
+        System.out.println(mcmf(src, snc));
+    }
+
+    static void addedge(int u, int v, int w) {
+        Edge edge = new Edge(v, 1, w);
+        Edge rev = new Edge(u, 0, -w);
+        edge.rev = rev;
+        rev.rev = edge;
+        con[u].add(edge);
+        con[v].add(rev);
+    }
+
+    static int mcmf(int src, int snc) {
+        int res = 0, flow = 0;
+        int[] par = new int[42];
+        int[] d = new int[42];
+        boolean[] in = new boolean[42];
+        Edge[] trace = new Edge[42];
+
+        while (true) {
+            Arrays.fill(par, -1);
+            Arrays.fill(d, -1);
+            Arrays.fill(in, false);
+            d[src] = 0;
+            Queue<Integer> q = new LinkedList<>();
+            q.add(src);
+
+            while (!q.isEmpty()) {
+                int u = q.poll();
+                in[u] = false;
+
+                for (int i = 0; i < con[u].size(); i++) {
+                    Edge edge = con[u].get(i);
+                    int v = edge.v, w = edge.w;
+
+                    if (!edge.pos()) continue;
+
+                    if (par[v] < 0 || (d[v] < 0 || d[v] > d[u] + w)) {
+                        par[v] = u;
+                        d[v] = d[u] + w;
+                        trace[v] = edge;
+
+                        if (!in[v]) {
+                            in[v] = true;
+                            q.add(v);
+                        }
+                    }
+                }
+            }
+
+            if (d[snc] < 0) return res;
+            res += d[snc];
+            flow++;
+
+            for (int cur = snc; cur != src; cur = par[cur])
+                trace[cur].flow(1);
+        }
     }
 }
