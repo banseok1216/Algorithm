8601ef7512d8cf7f249fe9be5fc2ca5e94421a23
banseok1216 <114564687+banseok1216@users.noreply.github.com>
2024-03-08 18:14:59 +0900

    [Gold V] Title: A와 B, Time: 132 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -1,40 +1,33 @@
-
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-if __name__ == '__main__':
+def is_possible(list1, list2):
+    que = deque(list2)
     front = True
+    while len(que) != len(list1):
+        if front:
+            if que[-1] == "B":
+                front = False
+            que.pop()
+        else:
+            if que[0] == "B":
+                front = True
+            que.popleft()
+
+    for char in list1:
+        if front:
+            if char != que.popleft():
+                return 0
+        else:
+            if char != que.pop():
+                return 0
+
+    return 1
+
+if __name__ == '__main__':
     list1 = list(input().rstrip())
     list2 = list(input().rstrip())
-    que = deque()
-    for i in range(len(list2)):
-        que.append(list2[i])
-    while(len(que) != len(list1)):
-        if(front):
-            if (que[-1] == "B"):
-                que.pop()
-                front = False
-            elif  (que[-1] == "A"):
-                que.pop()
-        else:
-            if (que[0] == "B"):
-                que.popleft()
-                front = True
-            elif  (que[0] == "A"):
-                que.popleft()
-    count =0
-    ans = 1
-    while(que):
-        if (front):
-            if list1[count] != que.popleft():
-                ans = 0
-                break
-            count +=1
-        else:
-            if list1[count] != que.pop():
-                ans = 0
-                break
-            count += 1
-    print(ans)
 
+    print(is_possible(list1, list2))
