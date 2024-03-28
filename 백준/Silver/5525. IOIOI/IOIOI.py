3d137d6fcd8c800c0eb0e99f783a84f34c7e37bf
banseok1216 <114564687+banseok1216@users.noreply.github.com>
2024-03-28 17:24:23 +0900

    [Silver I] Title: IOIOI, Time: 148 ms, Memory: 113120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,29 +1,17 @@
 N = int(input())
-first = "I"
-for i in range(N):
-    first+="O"+"I"
 M = int(input())
-lgt = input()
-start = 0
-ans = 0
-success = False
-for i in range(M-len(first)+1):
-    if success:
-        if i+2 <= M-len(first) and lgt[i+1] == "O" and lgt[i+2] == "I":
-            ans+=1
-            i +=2
-        else:
-            success = False
-            continue
-    if first[0] == lgt[i]:
-        start = i
-        ans +=1
-        success = True
-        for j in range(len(first)):
-            if first[j] != lgt[i+j]:
-               ans-=1
-               success = False
-               break
-        i = start +len(first)-1
-print(ans)
+S = input()
+answer, i, count = 0, 0, 0
 
+while i < (M - 1):
+    if S[i:i+3] == 'IOI':
+        i += 2
+        count += 1
+        if count == N:
+            answer += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
+
+print(answer)
