3980663bd28d569010834a90716d29be5f5251fa
banseok1216 <114564687+banseok1216@users.noreply.github.com>
2024-08-20 15:37:04 +0900

    [Gold II] Title: 물대기, Time: 176 ms, Memory: 116984 KB -BaekjoonHub

---
+++ 백준/Gold/1368. 물대기/물대기.py
@@ -0,0 +1,44 @@
+import sys
+
+input = sys.stdin.readline
+MAX = float('inf')
+
+N = int(input())
+road_list = list()
+
+for i in range(N):
+    well_cost = int(input())
+    road_list.append((well_cost, i + 1, 0))
+
+for i in range(N):
+    road_cost = list(map(int, input().split()))
+    for j in range(i + 1, N):
+        road_list.append((road_cost[j], i + 1, j + 1))
+
+road_list.sort()
+parent = list(range(N + 1))
+
+
+def find(a):
+    if a == parent[a]:
+        return parent[a]
+    parent[a] = find(parent[a])
+    return parent[a]
+
+
+def union(pa, pb):
+    if pa < pb:
+        parent[pb] = pa
+    else:
+        parent[pa] = pb
+
+
+answer = 0
+for cost, a, b in road_list:
+    pa = find(a)
+    pb = find(b)
+    if pa != pb:
+        union(pa, pb)
+        answer += cost
+
+print(answer)
