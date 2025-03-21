08d170f59f9c507bb6884f610af1c57d18980ffd
banseok1216 <114564687+banseok1216@users.noreply.github.com>
2025-03-21 23:08:00 +0900

    [level 3] Title: 에어컨, Time: 53.05 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/214289. 에어컨/에어컨.py
+++ 프로그래머스/3/214289. 에어컨/에어컨.py
@@ -1,44 +1,13 @@
-def solution(temperature, t1, t2, a, b, onboard):
-    mintemp, maxtemp = t1, t2
-    
-    dp = [[1e9] * 51 for _ in range(1001)]
-    
-    dp[0][temperature+10] = 0
-    
-    for t, is_onboard in enumerate(onboard[1:], 1):
-        if is_onboard:
-            mintemp_to_consider = mintemp
-            maxtemp_to_consider = maxtemp + 1
-        else:
-            mintemp_to_consider = -10
-            maxtemp_to_consider = 40 + 1
-            
-        for temp in range(mintemp_to_consider, maxtemp_to_consider):
-            if temp == temperature:
-                candidates = [dp[t-1][temp+10]]
-                if temp + 10 != 0:
-                    candidates.append(dp[t-1][temp-1+10])
-                if temp + 10 != 50:
-                    candidates.append(dp[t-1][temp+1+10])
-                    
-                dp[t][temp+10] = min(candidates)
-                
-            elif temp > temperature:
-                candidates = [dp[t-1][temp+10] + b]
-                if temp + 10 != 0:
-                    candidates.append(dp[t-1][temp-1+10] + a)
-                if temp + 10 != 50:
-                    candidates.append(dp[t-1][temp+1+10])
-                    
-                dp[t][temp+10] = min(candidates)
-            else:
-                candidates = [dp[t-1][temp+10] + b]
-                if temp + 10 != 0:
-                    candidates.append(dp[t-1][temp-1+10])
-                if temp + 10 != 50:
-                    candidates.append(dp[t-1][temp+1+10] + a)
-                    
-                dp[t][temp+10] = min(candidates)
-
-    answer = min(dp[len(onboard) - 1])
-    return answer
+def solution(T, T1, T2, a, b, onboard):
+    if T<T1:
+        T,T1,T2 = -T,-T2,-T1
+    N = len(onboard)
+    DP = [[1e9]*100 for i in range(N)]; DP[0][T]=0
+    for n in range(1,N):
+        for t in range(T1,T+1):
+            if onboard[n] and not T1<=t<=T2:
+                continue
+            DP[n][t] = min(DP[n-1][t-1],DP[n-1][t+1]+a,DP[n-1][t]+b)
+        if not onboard[n]:
+            DP[n][T] = min(DP[n][T],DP[n-1][T])
+    return min(DP[-1])
